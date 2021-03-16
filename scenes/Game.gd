grorp/coronagame

commit 35fbfac2dc83e968ea705f45809fd7ffa738de79

Remove more Denglish

scenes/Game.gd
@@ -9,15 +9,17 @@ func _input(event):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().change_scene("res://scenes/MainMenu.tscn")
 
-func _on_SpritzenSpawnTimer_timeout():
+func _on_SyringeSpawnTimer_timeout():
 	$MobSpawnPath/MobSpawnLocation.offset = randi()
 	var syringe = Syringe.instance()
 	add_child(syringe)
+	
 	syringe.position = $MobSpawnPath/MobSpawnLocation.position
 	var offset =  $Player/Camera2D.get_camera_screen_center()
 	offset.x -= get_viewport().size.x / 2
 	offset.y -= get_viewport().size.y / 2
 	syringe.position += offset
+	
 	var direction = $MobSpawnPath/MobSpawnLocation.rotation + PI / 2
 	direction += rand_range(-PI / 4, PI / 4)
 	syringe.rotation = direction

scenes/Game.tscn
@@ -23,8 +23,8 @@ Syringe = ExtResource( 3 )
 
 [node name="Player" parent="." instance=ExtResource( 1 )]
 
-[node name="SpritzenSpawnTimer" type="Timer" parent="."]
 wait_time = 0.5
+[node name="SyringeSpawnTimer" type="Timer" parent="."]
 autostart = true
 
 [node name="MobSpawnPath" type="Path2D" parent="."]
@@ -74,5 +74,5 @@ __meta__ = {
 }
 [connection signal="dead" from="Player" to="." method="_on_Player_dead"]
 [connection signal="victim_infected" from="Player" to="." method="_on_Player_victim_infected"]
-[connection signal="timeout" from="SpritzenSpawnTimer" to="." method="_on_SpritzenSpawnTimer_timeout"]
+[connection signal="timeout" from="SyringeSpawnTimer" to="." method="_on_SyringeSpawnTimer_timeout"]
 [connection signal="timeout" from="AfterGameOverTimer" to="." method="_on_AfterGameOverTimer_timeout"]

commit 107792a51b44df5150dfa202c52050fdb4459db2

Fix a bug introduced in 35fbfac2dc83e968ea705f45809fd7ffa738de79

scenes/Game.gd
@@ -27,7 +27,7 @@ func _on_SyringeSpawnTimer_timeout():
 	syringe.linear_velocity = syringe.linear_velocity.rotated(direction)
 
 func _on_Player_dead(enemy):
-	$SpritzenSpawnTimer.stop()
+	$SyringeSpawnTimer.stop()
 	enemy.linear_velocity = Vector2(0, 0)
 	$HUD/GameOverTxt.show()
 	$AfterGameOverTimer.start()