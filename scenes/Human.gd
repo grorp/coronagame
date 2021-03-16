e0aa1796f74293c9ba64218461b129ab650f0953
Gregor Parzefall <gregor.parzefall@posteo.de>
2021-03-16 11:58:12 +0100

    Create a completely new animation for infected humans

--- scenes/Human.gd
+++ scenes/Human.gd
@@ -4,8 +4,4 @@ var infected = false
 
 func infect():
 	infected = true
-	$AnimationPlayer.play("disappear")
-
-func _on_AnimationPlayer_animation_finished(anim_name):
-	if anim_name == "disappear":
-		queue_free()
+	$AnimationPlayer.play("infect")
