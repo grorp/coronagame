35fbfac2dc83e968ea705f45809fd7ffa738de79
Gregor Parzefall <gregor.parzefall@posteo.de>
2021-03-16 10:35:28 +0100

    Remove more Denglish

--- scenes/Game.gd
+++ scenes/Game.gd
@@ -9,15 +9,17 @@ func _input(event):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().change_scene("res://scenes/MainMenu.tscn")
 
-func _on_SpritzenSpawnTimer_timeout():
+func _on_SyringeSpawnTimer_timeout():
 	$MobSpawnPath/MobSpawnLocation.offset = randi()
 	var syringe = Syringe.instance()
 	add_child(syringe)
+	
 	syringe.position = $MobSpawnPath/MobSpawnLocation.position
 	var offset =  $Player/Camera2D.get_camera_screen_center()
 	offset.x -= get_viewport().size.x / 2
 	offset.y -= get_viewport().size.y / 2
 	syringe.position += offset
+	
 	var direction = $MobSpawnPath/MobSpawnLocation.rotation + PI / 2
 	direction += rand_range(-PI / 4, PI / 4)
 	syringe.rotation = direction
