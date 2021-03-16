aefb67a1e0d97a235221640938b62a086f251340
Gregor Parzefall <gregor.parzefall@posteo.de>
2021-03-16 19:18:21 +0100

    Fix some problems in and do some improvements to the difficulty selection menu

--- scenes/DifficultyMenu.gd
+++ scenes/DifficultyMenu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+func _input(event):
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().change_scene("res://scenes/MainMenu.tscn")
+
 func _on_EasyButton_pressed():
 	Global.difficulty = Global.Difficulty.EASY
 	get_tree().change_scene("res://scenes/MainMenu.tscn")
